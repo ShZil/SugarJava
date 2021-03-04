eaab3145e73307ad07a58d4ea82e167043a019c3
ShZil <shzil333@gmail.com>
2021-03-04 21:46:28 +0200

    Good, good.

--- Main.java
+++ Main.java
@@ -6,144 +6,140 @@ import java.util.Scanner; // Import the Scanner class to read text files
 import java.util.Arrays; // import the Arrays class
 
 public class Main {
-  public static void main(String[] args) {
-    String[] code = readFile("code.txt").split("\n");
-    code = solveForOver(code);
-    code = solveArrayAccesses(code);
-    System.out.println(Arrays.deepToString(code));
-    writeToFile("compiled.txt", code);
-  }
+    public static void main(String[] args) {
+        String[] code = readFile("code.txt").split("\n");
+        code = solveForOver(code);
+        code = solveArrayAccesses(code);
+        System.out.println(Arrays.deepToString(code));
+        writeToFile("compiled.txt", code);
+        // solveRegex("for (int index over arr)", "*for*(int 0 over 0 )*");
+    }
 
-  public static String[] solveForOver(String[] code) {
-    System.out.println("solveForOver:");
-    // for (type variable over array)
-    // for (E e over a) -> min length = 16
-    // for (type index = 0; index < array.length; index++)
-    for (int i = 0; i < code.length; i++) {
-      if (code[i].length() < 16) {
-        continue;
-      }
-      if (code[i].startsWith("for")) {
-        String[] words = separateWords(code[i]);
-        if (words[3].startsWith("over")) {
-          String type = words[1];
-          String index = words[2];
-          String array = words[4];
-          code[i] = "for (" + type + " " + index + " = 0; " + index + " < " + array + ".length; " + index + "++) {";
+    public static String[] solveForOver(String[] code) {
+        System.out.println("solveForOver:");
+        // for (int index over array)
+        for (int i = 0; i < code.length; i++) {
+            String[] names = solveRegex(code[i], "for (int 0 over 0)");
+            if (names == null) continue;
+            code[i] = substitute("for (int {0} = 0; {0} < {1}.length; {0}++)", names);
         }
-      }
+        return code;
     }
-    return code;
-  }
 
-  public static String[] solveArrayAccesses(String[] code) {
-    System.out.println("solveArrayAccesses:");
-    // _____array@index______
-    // _____array[index]_____
-    for (int i = 0; i < code.length; i++) {
-      String[] words = separateWords(code[i]);
-      for (int j = 0; j < words.length; j++) {
-        if (contains(words[j].toCharArray(), '@')) {
-          System.out.println("Word found = " + words[j]);
-          String[] expression = words[j].split("@");
-          String array = expression[0];
-          String index = expression[1];
-          code[i] = code[i].replace(words[j], array + "[" + index + "]");
+    public static String[] solveArrayAccesses(String[] code) {
+        System.out.println("solveArrayAccesses:");
+        // array@index
+        for (int i = 0; i < code.length; i++) {
+            String[] names = solveRegex(code[i], "0@0 0;");
+            if (names == null) continue;
+            code[i] = substitute("{0}[{1}] {2};", names);
         }
-      }
+        return code;
     }
-    return code;
-  }
 
-  public static String readFile(String path) {
-    String out = "";
-    try {
-      File file = new File(path);
-      Scanner scan = new Scanner(file);
-      while (scan.hasNextLine()) {
-        out += scan.nextLine().trim();
-        out += "\n";
-      }
-      scan.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
-    }
-    return out;
-  }
-
-  public static String[] separateWords(String line) {
-    char[] nonWordCharacters = " {}();".toCharArray();
-    String[] out = new String[10];
-    for (int o = 0; o < out.length; o++) {
-      out[o] = "";
-    }
-    int index = 0;
-    for (int i = 0; i < line.length(); i++) {
-      if (contains(nonWordCharacters, line.charAt(i))) {
-        if (out[index].length() > 0) {
-          index++;
+    public static String readFile(String path) {
+        String out = "";
+        try {
+            File file = new File(path);
+            Scanner scan = new Scanner(file);
+            while (scan.hasNextLine()) {
+                out += scan.nextLine().trim();
+                out += "\n";
+            }
+            scan.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
-      } else {
-        out[index] += line.charAt(i);
-      }
+        return out;
     }
-    return out;
-  }
 
-  public static boolean contains(char[] chars, char c) {
-    for (char ch : chars) {
-      if (ch == c) {
-        return true;
-      }
+    public static void writeToFile(String path, String[] data) {
+        try {
+            File file = new File(path);
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        try {
+            FileWriter write = new FileWriter(path);
+            write.write(String.join("\n", data));
+            write.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
-    return false;
-  }
 
-  public static void writeToFile(String path, String[] data) {
-    try {
-      File file = new File(path);
-      if (file.createNewFile()) {
-        System.out.println("File created: " + file.getName());
-      } else {
-        System.out.println("File already exists.");
-      }
-    } catch (IOException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
+    public static String[] solveRegex(String line, String regex) {
+        final char all = '*';
+        final char parameter = '0';
+        final char any = '_';
+
+        int index = 0;
+        int regexIndex = 0;
+        int parameterIndex = 0;
+        boolean matched = true;
+        String[] parameters = new String[5];
+        Arrays.fill(parameters, "");
+        int length = line.length();
+
+        if (regex.charAt(regex.length()-1) != ';') {
+            System.out.println("WARNING: Make sure you have a closing character. \"" + regex + "\"");
+        }
+
+        while (index < length) {
+            char current = line.charAt(index);
+            if (regexIndex >= regex.length()) break;
+            char match = regex.charAt(regexIndex);
+
+            if (match == all || match == parameter) {
+                boolean isParameter = match != all;
+                char nextMatch = regex.charAt(regexIndex + 1);
+                while (current != nextMatch) {
+                    index++;
+                    if (index > length) {
+                        matched = false;
+                        break;
+                    }
+                    if (isParameter) parameters[parameterIndex] += current;
+                    if (index >= length) {
+                        System.out.println("Uhh... I ran out of characters to check...");
+                        matched = false;
+                        break;
+                    }
+                    current = line.charAt(index);
+                }
+                regexIndex++;
+                if (isParameter) parameterIndex++;
+            } else if (match == any) {
+                index++;
+                regexIndex++;
+                continue;
+            } else if (match != current) {
+                System.out.println(match + " should match " + current);
+                matched = false;
+                break;
+            }
+            index++;
+            regexIndex++;
+        }
+        System.out.println(matched);
+        if (matched) System.out.println(Arrays.deepToString(parameters));
+        if (matched) return parameters;
+        return null;
     }
-    try {
-      FileWriter write = new FileWriter(path);
-      write.write(String.join("\n", data));
-      write.close();
-      System.out.println("Successfully wrote to the file.");
-    } catch (IOException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
+
+    public static String substitute(String format, String[] params) {
+        for (int i = 0; i < params.length; i++) {
+            format = format.replaceAll("\\{" + i + "\\}", params[i]);
+        }
+        return format;
     }
-  }
 }
-
-
-// public static String[] solveRegex(String line, String regex)
-// if line doesn't match regex, return null.
-// String regex = "for ({0} over {1}) *"
-// Check if line matches regex:
-// int regexIndex = 0, index = 0;
-// while (index < length) {
-//    char current = line.charAt(index)
-//    char match = regex.charAt(regexIndex)
-//    if (match == '*') {
-//       while (current != nextMatch) {
-//          index++
-//          reEval current
-//          if (index > length) {
-//            matched = false;
-//            break;
-//          }
-//       }
-//    }
-//    else if (match != regex) {
-//
-//    }
-// }
--- README.md
+++ README.md
@@ -1,14 +1,30 @@
 # SugarJava
 for over, array@index, and more sugar syntax!
 
+Loop over an array (or Iteritable) with an index simply.
 ```java
 for (index over array)
 ==
 for (int index = 0; index < array.length; index++)
 ```
 
+Access an array member simply, with @ meaning (and read as) "at".
 ```java
 array@index
 ==
 array[index]
+```
+
+Simplify and shorten the java print statement.
+```java
+print x;
+==
+System.out.println(x);
+```
+
+Extend the +=, -=, *=, /=, and %= syntax to other, custom or language given, methods.
+```java
+value = .someMethod();
+==
+value = value.someMethod();
 ```
